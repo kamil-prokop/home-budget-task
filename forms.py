4e222136a324b3b2ffcbee996b3edf52d00541e8
Kamil Prokop <zara.pug.yt@gmail.com>
2023-02-26 12:43:22 +0100

    form created

--- forms.py
+++ forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired
 
 class BudgetForm(FlaskForm):
-    position = StringField('pozycja', validators=[DataRequired()])
-    description = TextAreaField("opis", validators=[DataRequired()])
-    payment_status = BooleanField("zapłacone", validators=[DataRequired()])
+    pozycja = StringField("pozycja", validators=[DataRequired()])
+    opis = TextAreaField("opis", validators=[DataRequired()])
+    payment_status = BooleanField("payment_status", validators=[DataRequired()])
--- models.py
+++ models.py
@@ -1,30 +1,30 @@
 import json
 
-class Budget:
+class Budgets():
     def __init__(self):
         try:
-            with open("budget.json", "r") as f:
-                self.budget = json.load(f)
+            with open("budget.json", "r", encoding="UTF-8") as f:
+                self.budgets = json.load(f)
         except FileNotFoundError:
-            self.budget = []
+            self.budgets = []
 
     def all(self):
-        return self.budget
+        return self.budgets
     
     def get(self, id):
-        return self.budget[id]
+        return self.budgets[id]
     
     def create(self, data):
         data.pop("csrf_token")
-        self.budget.append(data)
+        self.budgets.append(data)
 
     def save_all(self):
-        with open("budget.json", "w") as f:
-            json.dump(self.budget, f)
+        with open("budget.json", "w", encoding="UTF-8") as f:
+            json.dump(self.budgets, f)
     
     def update(self, id, data):
         data.pop("csrf_token")
-        self.budget[id] = data
+        self.budgets[id]=data
         self.save_all()
     
-budget = Budget()
+budgets = Budgets()
